be4037deb26c3cde7ed93d73943a812297aafd6f
VihagaYohan <vihagayohan94@gmail.com>
2020-10-01 19:09:29 +0530

    2_test commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-com      <Text>Kade - An E-commerce Platform</Text>
+     <Text>Kade - An E-commerce Platform</Text>
       <StatusBar style="auto" />
     </View>
   );
